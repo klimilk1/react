cd458242743f612e1686c03647545a03e13617a4
klim <klim.borisov.2017@mail.ru>
2021-03-13 23:01:51 +0300

    React

---
+++ React_test/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+const App = 9 => {
+    return ( <
+        div className = "App" >
+        <
+        div className = "weatherContainer pt-3 pb-3" >
+        <
+        WeatherBody time = { 'Morning' }
+        icon = { 'Sun' }
+        minTemp = { 15 }
+        maxTemp = { 25 }
+        /> <
+        WeatherBody time = { 'day' }
+        icon = { 'Sun' }
+        minTemp = { 15 }
+        maxTemp = { 25 }
+        /> <
+        WeatherBody time = { 'evening' }
+        icon = { 'Sun' }
+        minTemp = { 15 }
+        maxTemp = { 25 }
+        /> <
+        WeatherBody time = { 'night' }
+        icon = { 'Sun' }
+        minTemp = { 15 }
+        maxTemp = { 25 }
+        /> < /
+        div >
+    ); < /div>
+};
+
+export defualt App;
